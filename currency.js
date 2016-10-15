ed568cb2a345701d20179847e7e3729d5b86848b
kxu <kxu@freewheel.tv>
2016-10-15 16:46:30 +0800

    add ajax for api fetching the latest data from yahoo finance

--- currency.js
+++ currency.js
@@ -1,23 +1,50 @@
+// currently we support 美元，欧元，港币，日元，英镑 as major currencies
+var targetCurrenciesName = ['USD', 'EUR', 'HKD', 'GBP', 'JPY'];
+var CurrencySymbol = {
+    'USD': '$',
+    'EUR': '€',
+    'HKD': 'HK$',
+    'GBP': '£',
+    'JPY': 'JP¥'
+};
+var quote;
+var responseData;
+var exchanges = [];
+
 function currencyExchange(sourceCurrencyNumber, exchange) {
-    var targetCurrencyNumber = [];
-    targetCurrencyNumber = exchange.map((x) => x * sourceCurrencyNumber);
-    return targetCurrencyNumber;
+    var targetCurrencies = [];
+    // should let the results only shown with 1 number after decimal point
+    exchange.forEach(function(elements, index, array) {
+        return targetCurrencies.push([targetCurrenciesName[index], (sourceCurrencyNumber * elements).toFixed(1), CurrencySymbol[targetCurrenciesName[index]]]);
+    });
+    return targetCurrencies;
 }
 
-$(function() {
-    var vm1 = new Vue({
-        el: '#inputNumber',
-        data: {
-            sourceCurrencyNumber: document.getElementById("inputNumber").value
-        }
+$().ready(function() {
+    // http://developer.yahoo.com/yql/console/?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22CNYUSD%22%2C%20%22CNYEUR%22%2C%20%22CNYHKD%22%2C%20%22CNYGBP%22%2C%20%22CNYJPY%22)&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
+    $.ajax({
+        url: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22CNYUSD%22%2C%20%22CNYEUR%22%2C%20%22CNYHKD%22%2C%20%22CNYGBP%22%2C%20%22CNYJPY%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=quote",
+        dataType: "jsonp",
+        jsonp: "callback",
+        jsonpCallback: "quote"
     });
-    var vm2 = new Vue({
-        el: '#currencies', // can use v-for to realize
-        computed: {
-            targetCurrencyNumber: function() {
-                let exchange = [1, 2, 6.7]; // need to obtain from api
-                return currencyExchange(vm1.sourceCurrencyNumber, exchange);
+    quote = function(data) {
+        responseData = data.query.results.rate;
+        // follow the sequence that targetCurrenciesName defines same as the response return to reduce process comlexity
+        exchanges = responseData.map((x) => x.Rate);
+        var vm1 = new Vue({
+            el: '#inputNumber',
+            data: {
+                sourceCurrencyNumber: document.getElementById("inputNumber").value
             }
-        }
-    });
+        });
+        var vm2 = new Vue({
+            el: '#currencies', // can use v-for to realize
+            computed: {
+                targetCurrencies: function() {
+                    return currencyExchange(vm1.sourceCurrencyNumber, exchanges);
+                }
+            }
+        });
+    };
 });
